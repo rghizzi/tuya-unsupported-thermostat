e8113101e5078bdb93aa7f368c7460cb8a1122ee
Maghiel Dijksman <mail@mdijksman.nl>
2025-03-21 11:23:21 +0100

    update manifest

--- custom_components/tuya-lsc_camera_fix/const.py
+++ custom_components/tuya-lsc_camera_fix/const.py
@@ -27,7 +27,7 @@ from homeassistant.const import (
     UnitOfVolume,
 )
 
-DOMAIN = "tuya"
+DOMAIN = "tuya-lsc_camera_fix"
 LOGGER = logging.getLogger(__package__)
 
 CONF_APP_TYPE = "tuya_app_type"
